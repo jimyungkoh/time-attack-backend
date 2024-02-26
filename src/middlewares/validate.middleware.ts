2de12f04f34dd34dfaa60fcc67f48fd49c27b7f4
jimyungkoh <kojimyung1206@naver.com>
2024-02-26 17:55:22 +0900

    feat: 유효성 검사 미들웨어 추가

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
+
+const validate =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+
+export default validate;
